fcee0cc46a5efad6d66641f9f45459b302c899fd
Saloni <saloninarayana1@gmail.com>
2025-06-28 10:54:23 +0530

    Added files routes to the main file

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import MapView from './components/MapView';
+
+function App() {
+    return(
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/map" element={<MapView />}/>
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
