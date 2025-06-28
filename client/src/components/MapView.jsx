9480ced92b879ac0a97d9b6db22ec79b9d0cce38
Saloni <saloninarayana1@gmail.com>
2025-06-28 12:10:16 +0530

    Added useEffect logic

--- client/src/components/MapView.jsx
+++ client/src/components/MapView.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ModeSelector from './ModeSelector';
 import SafetyPopup from './SafetyPopup';
 import ShareButton from './ShareButton';
@@ -8,6 +8,20 @@ function MapView() {
     const [score, setScore] = useState(null);
 
     const shareURL = window.location.href;
+
+    useEffect(() => {
+        if(window.tt && document.getElementById('map')) {
+            const map = window.tt.map({
+                key: import.meta.env.VITE_TOMTOM_API_KEY,
+                container: 'map',
+                center: [77.2090, 28.6139], // Delhi
+                zoom: 10,
+            });
+
+            map.addControl(new window.tt.NavigateControl());
+        }
+    }, []);
+    
 
     return(
         <div id="map" style={{ width: '100%', height: '400px' }}>
