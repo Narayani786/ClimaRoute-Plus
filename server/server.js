d64f94e0edeba8f349a0cb8baa3294801e1c1c13
Saloni <saloninarayana1@gmail.com>
2025-07-01 07:57:56 +0530

    changed api route

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import routeRoutes from './routes/route.js';
+import routeRouter from './routes/route.js';
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/route', routeRoutes);
+app.use('/api/route', routeRouter);
 
 const PORT = process.eventNames.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
